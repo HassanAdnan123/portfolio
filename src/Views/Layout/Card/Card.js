8f7a2bedd3dff7fd2f61778fc049fba663647a4d
Hassan Adnan <43107637+HassanAdnan123@users.noreply.github.com>
2022-10-20 13:14:17 +0500

    Improved UI and added more content

--- src/Views/Layout/Card/Card.js
+++ src/Views/Layout/Card/Card.js
@@ -6,7 +6,10 @@ export default function Card(props) {
             <h2>    {props.heading}  </h2>
                     {props.content}
                     {props.image}
+                    {props.fullwidthimageurl? <img className='fullWidth' src={props.fullwidthimageurl} /> : null }
             <div className='vertical-aligned'>
+            <h2>    {props.verticalAlignedSubHeading} </h2>
+            <h1>    {props.verticalAlignedMainHeading} </h1>
                     {props.verticalAlignedContent} 
             </div>
         </div>
--- src/Views/Portfolio.js
+++ src/Views/Portfolio.js
@@ -2,22 +2,41 @@ import React from 'react'
 import Card from './Layout/Card/Card'
 import Navbar from './Layout/Navbar/Navbar'
 import DeveloperImage from './Custom/DeveloperImage'
+import tech_stack from './../Assets/tech_stack.png'
+import { Carousel } from 'primereact/carousel';
 import './Portfolio.css'
 import LandingText from './Custom/LandingText'
 
 export default function Portfolio() {
+  const itemTemplate = (item) => {
+    return <Card verticalAlignedSubHeading = {item.title} verticalAlignedContent = {item.content}/>
+}
+  const projects = [
+    {title: "TBML Implementation",
+    content: "Implemented the state bank's trade based anti money laundering module."},
+    {title: "Connected 4 Life",
+    content: "Developed the backend of a healthcare application that is based on emergency rescue services."},
+    {title: "Human Capital Management",
+    content: "Built the architecture of a Human Resource management system in Java Springboot"},
+    {title: "Attendance Portal",
+    content: "Developed a full stack app for attendance management for a local client"}
+  ]
+
   return (
     <>
       <div className='navbar-container'><Navbar/></div>
       <div className='cards'>
             <Card image={<DeveloperImage/>}/>
             <Card verticalAlignedContent={<LandingText/>} />
-            <Card heading="MEAN, MERN & PERN are my favorite technology stacks!" 
-                  content={ 
+            <Card verticalAlignedSubHeading="MEAN, MERN & PERN are my favorite technology stacks!" 
+                  verticalAlignedContent={ 
                             <p> MySQL / Postgres, Express, Angular / React and Node are
                                 my relatively strong suites.
                             </p> 
                           } />
+            <Card fullwidthimageurl={tech_stack}/>
+            <Card verticalAlignedMainHeading="Projects"/>
+            <Card content= {<Carousel value={projects} itemTemplate={itemTemplate}></Carousel> }/>
       </div>
     </>
   )
