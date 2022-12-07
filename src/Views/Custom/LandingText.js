473a95030f038ea25511e0150e8995e84d0ad2dc
Hassan Adnan <43107637+HassanAdnan123@users.noreply.github.com>
2022-12-07 17:29:30 +0500

    added animation on landing text

--- src/Views/Custom/LandingText.js
+++ src/Views/Custom/LandingText.js
@@ -1,11 +1,12 @@
 import './../Portfolio.css'
+import './LandingText.css'
 
 export default function LandingText(){
     return (
         <div>
             <h1 className='landingText'>
                 <code> &lt;code&gt; </code>
-                    Hi! 👋<br/> &nbsp; I am Hassan, <br/>&nbsp; Full Stack Developer..
+                    Hi! 👋<br/> &nbsp; I am Hassan, <br/>&nbsp; <span className='animate-flicker'>Full Stack Developer..</span>
                 <code> &lt;code/&gt; </code>
             </h1>
         </div>
