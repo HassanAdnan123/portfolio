e272b028cec86f73037eb351b580789e8fc04058
Hassan Adnan <43107637+HassanAdnan123@users.noreply.github.com>
2022-12-07 18:09:38 +0500

    typewriter text added

--- src/Views/Custom/LandingText.js
+++ src/Views/Custom/LandingText.js
@@ -1,12 +1,26 @@
 import './../Portfolio.css'
 import './LandingText.css'
+import TypeWriterEffect from 'react-typewriter-effect'
 
 export default function LandingText(){
     return (
         <div>
             <h1 className='landingText'>
                 <code> &lt;code&gt; </code>
-                    Hi! 👋<br/> &nbsp; I am Hassan, <br/>&nbsp; <span className='animate-flicker'>Full Stack Developer..</span>
+                    <br/> Hi! 👋 I am Hassan, <TypeWriterEffect
+                    textStyle={{ fontFamily: 'IBM Plex Sans', fontSize: '42px' }}
+                    startDelay={100}
+                    cursorColor="grey"
+                    multiText={[
+                        'I am a Full Stack Developer.',
+                        'I explore wonders of physics casually 🧑‍🔬',
+                        'I love Javascript 🤩',
+                        'I read History in my leisure 🌍',
+                      ]}
+                    multiTextDelay={1000}
+                    typeSpeed={100}
+                    eraseSpeed={100} />
+                {/* </span> */}
                 <code> &lt;code/&gt; </code>
             </h1>
         </div>
