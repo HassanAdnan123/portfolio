1e79768573bdb5e5464ce204d676fcb0133064d2
Hassan Adnan <43107637+HassanAdnan123@users.noreply.github.com>
2024-10-09 19:59:12 +0500

    Issue #5 : Skeleton loader on icons

--- src/Views/Layout/Icons/Icon.js
+++ src/Views/Layout/Icons/Icon.js
@@ -1,33 +1,44 @@
-import { useState, useEffect } from 'react'
-import './Icons.css'
+import { useState, useEffect } from 'react';
+import { Skeleton } from 'primereact/skeleton';
+import './Icons.css';
+import 'primereact/resources/themes/saga-blue/theme.css';   // Choose your theme
+import 'primereact/resources/primereact.min.css';           // PrimeReact styles
+import 'primeicons/primeicons.css';                        // PrimeIcons
 
 export default function Icon(props) {
-  const [image, setImage] = useState(null)
+  const [image, setImage] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false); // Track loading state
 
   useEffect(() => {
     const fetchImage = async () => {
-        try {
-            const response = await import(`../../../Assets/Icons/${props.name}.png`) 
-            setImage(response.default)
-        } catch (err) {
-            console.log("Unable to load icon")
-        }
-    }
-    fetchImage()
-  }, [props.name])
+      try {
+        const response = await import(`../../../Assets/Icons/${props.name}.png`);
+        setImage(response.default);
+      } catch (err) {
+        console.log("Unable to load icon");
+      }
+    };
+    fetchImage();
+  }, [props.name]);
 
+  // Handle image load
+  const handleImageLoad = () => {
+    setIsLoaded(true);
+  };
 
   return (
-    props.technologyIcon ?
-    <div className='iconContainer'>
-        <img className='technologyIcon' 
-            src={image} alt="icon"/>
+    <div className={isLoaded ? `iconContainer` : `skeletonContainer`}>
+      {!isLoaded && <Skeleton className='skeleton skeleton-mode' shape="circle" />} {/* Placeholder */}
+      {image && (
+        <img
+          className={props.technologyIcon ? 'technologyIcon' : 'small'}
+          src={image}
+          alt="icon"
+          onLoad={handleImageLoad}
+          style={{ display: isLoaded ? 'block' : 'none' }} // Hide until loaded
+        />
+      )}
+      {isLoaded && !props.technologyIcon && <p>{props.title}</p>}
     </div>
-    : 
-    <div className='iconContainer'>
-        <img className='small' 
-            src={image} alt="icon"/>
-        <p>{props.title}</p>
-    </div>
-  )
+  );
 }
